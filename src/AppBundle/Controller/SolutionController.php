081f1c08ea98880ee9d9a139ad10a45cc589d92a
Zamirbek Abdyldaev <sampobek@gmail.com>
2018-01-05 11:29:30 +0600

    #21 Solution page

--- src/AppBundle/Controller/SolutionController.php
+++ src/AppBundle/Controller/SolutionController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SolutionController extends Controller
 {
@@ -21,5 +22,17 @@ class SolutionController extends Controller
         return $this->render('@App/Solution/index.html.twig', array(
             'solutions' => $solutions,
         ));
+    }
+
+	/**
+	 * @Route("/test", name="test")
+	 */
+    public function testAction()
+    {
+	    $pusher = $this->container->get('gos_web_socket.wamp.pusher');
+//push(data, route_name, route_arguments)
+	    $pusher->push(['my_data' => 'data'], 'app_topic_chat');
+
+	    return new JsonResponse();
     }
 }
